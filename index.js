21943c972f9c6a07b62ec3f94addf3e98b2d5d04
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2019-11-30 16:43:03 -0500

    updating put request

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require('cors')
 const app = express();
 const parser = require("body-parser");
-const Directoryroute = require("./lib/routes/directory")
+const Directoryroute = require("../lib/routes/directory")
 
 app.use(parser.json())
 app.use(Directoryroute)
@@ -11,14 +11,9 @@ app.use(cors())
 // app.listen(3000, () =>
 //   console.log('Betur not check for me, unless youve got a check for me'))
 
-// app.set("port", process.env.PORT || 8080);
+app.set("port", process.env.PORT || 8080);
 
-// app.listen(app.get("port"), () => {
-//     console.log(`✅ PORT: ${app.get("port")} 🌟`);
-//   });
-
-
-  app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+app.listen(app.get("port"), () => {
+    console.log(`✅ PORT: ${app.get("port")} 🌟`);
   });
 
